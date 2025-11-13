e56e22772b9c85d7e133b7566d1c3aa4e5dc55c7
Your Name <you@example.com>
2025-11-13 18:51:16 +0530

    moved biharfoss files to root folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return(
         <section id="home" className="hero">
         <div className="hero-content">
-            <img src="hero-globe.png" alt="" />
+            <img height="250px" width="250px" src="hero-globe.png" alt="" />
             <h1> Growing Bihar's FOSS Ecosystem</h1>
             <p>Building a vibrant open source community in Bihar. Join us to learn, contribute, and grow together.</p>
             <a href="#events" className="cta-button">Explore FOSS Events</a>
